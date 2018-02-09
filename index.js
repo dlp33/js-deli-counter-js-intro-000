aabe91db1e8eb8ad43afacb75fa1cfcd335b3465
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-09 20:45:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName);
   var name = katzDeliLine[katzDeliLine.length - 1];
   var place = katzDeliLine.length - 1;
-  return "Welcome, " + name + ". You are number 88 in line." ;
+  return "Welcome, " + name + ". You are number " + place + " in line." ;
 }
