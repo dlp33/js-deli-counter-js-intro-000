f840ab179911ac7bcc14e1ad3461b6e48031c9e7
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-09 21:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function nowServing(array) {
 function currentLine(line) {
   var lineString = "The line is currently: ";
     for(var i = 0; i < line.length -1; i++) {
-      lineString += (i+1) + ". " + line[i] + ",";
+      lineString += (i+1) + ". " + line[i] + ", ";
     }
 
     return lineString;
